fb44e231b6c9af90d6fada2671c2b5b38ff89c41
Summer <511391805@qq.com>
2022-03-29 14:58:10 +0800

    :seedling:配置MybatisPlus

--- src/main/java/com/example/paymentdemo/controller/ProductController.java
+++ src/main/java/com/example/paymentdemo/controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.example.paymentdemo.controller;
 
 
+import com.example.paymentdemo.entity.Product;
+import com.example.paymentdemo.service.ProductService;
+import com.example.paymentdemo.vo.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -13,8 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Summer
  * @since 2022-03-29
  */
+@Api(tags = "商品管理")
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
+    @Resource
+    private ProductService productService;
+    @ApiOperation("商品列表")
+    @GetMapping()
+    public R index(){
+        List<Product> list = productService.list();
+        return R.ok().data("products",list);
+    }
 }
